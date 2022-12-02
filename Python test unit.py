03fadba1c464c71bda4240c995e396722bd61978
ZzONEx <mnakosergej@gmail.com>
2022-12-02 17:40:05 +0300

    тест функции

--- Python test unit.py
+++ Python test unit.py
@@ -1,27 +1,5 @@
-from main import line
 import unittest
-
+from main import count_vowels
 class TestReverse(unittest.TestCase):
     def test_empty(self):
-        self.assertEqual(line(""),"")
-
-    def test_one_sym(self):
-        self.assertEqual(line("a"), "a")
-
-    def test_polindrom(self):
-        self.assertEqual(line("aba"), "aba")
-
-    def test_correct(self):
-        self.assertEqual(line("abcd"), "dcba")
-
-    def test_type_int(self):
-        with self.assertRaises(TypeError):
-            line(42)
-
-    def test_type_list(self):
-        with self.assertRaises(TypeError):
-            line('c', 'a', 'd')
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(count_vowels(''), '')
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ line = input("Введите строку:").lower()
 count = 0
 count_vowels = set("УЕЫАОЭЯИЮуеыаоэяию")
 for letter in line:
-    if letter in count_vowels:
+    if letter.lower() in count_vowels:
         count += 1
 print("")
 print(count)
