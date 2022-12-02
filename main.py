39cd0ecef2bd4080688b7f8028b5a2d8dc1b0394
ZzONEx <mnakosergej@gmail.com>
2022-12-02 16:17:30 +0300

    Первй коммит

---
+++ main.py
@@ -0,0 +1,8 @@
+line = input("Введите строку").lower()
+count = 0
+count_vowels = set("УЕЫАОЭЯИЮуеыаоэяию")
+for letter in line:
+    if letter in count_vowels:
+        count += 1
+print("")
+print(count)
